ab6bfc0a75e39a87115d9d1d374f9df48159921c
tctao <tctao@iflytek.com>
2021-01-25 16:40:17 +0800

    upgrade: enzyme

---
+++ jest.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "collectCoverageFrom": [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts"
+  ],
+  "setupFiles": [
+    "react-app-polyfill/jsdom"
+  ],
+  // 测试环境搭建好后执行的文件
+  "setupFilesAfterEnv": [
+    "<rootDir>/src/setupTests.js",
+    './node_modules/jest-enzyme/lib/index.js'
+  ],
+  "testMatch": [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+  ],
+  // 测试环境：在 node 环境中模拟 dom api
+  "testEnvironment": "jsdom",
+  "testRunner": "C:\\Users\\tctao\\Desktop\\Web Projects\\HelloWeb\\jest-react\\node_modules\\jest-circus\\runner.js",
+  "transform": {
+    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+  },
+  "transformIgnorePatterns": [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  "modulePaths": [],
+  "moduleNameMapper": {
+    "^react-native$": "react-native-web",
+    // identity-obj-proxy 是一个第三方模块，在测试文件中引入 css module 文件时，该模块会将原始样式转换成一个对象
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+  },
+  // 引入模块时，如没有写后缀，会自动搜索的文件后缀类型
+  "moduleFileExtensions": [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node"
+  ],
+  // 增加运行 jest 时可选的模式
+  "watchPlugins": [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ],
+  "resetMocks": true
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from './App';
 
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const div = document.createElement('div')
+  // ReactDOM.render(<App />, div)
+  // const app = div.getElementsByClassName('App')
+  // expect(app.length).toBe(1)
+
+  // mount：渲染 App 组件及其子组件。
+  // shallow：只渲染 App 组件本身，而不会渲染 App 中引入的子组件，速度较快，适用于组件的单元测试。
+  const wrapper = shallow(<App />)
+
+  console.log(wrapper.debug())
+  // expect(wrapper.find('.App').length).toBe(1)
+  // expect(wrapper.find('.App').prop('title')).toBe('test')
+  // 避免使用类选择器，使测试用例与代码解耦。
+  const app = wrapper.find('[data-test="app"]')
+  expect(app.length).toBe(1)
+  // 安装 jest-enzyme，可以使用更多的匹配器
+  expect(app).toExist()
+  expect(app).toHaveProp('title', 'test')
+  // expect(app).toMatchSnapshot()
 });
